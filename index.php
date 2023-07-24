d07da2e0e326c873052166601a0e321d36c73c8e
Jerry May A. Alor <alorjerrymay@gmail.com>
2023-07-25 02:50:11 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link rel="stylesheet" href="style.css">
+     <title>Document</title>
+</head>
+
+<body>
+     <header class="header">
+          HEADER
+     </header>
+     <main class="main">
+          <aside class="aside">Side Menu</aside>
+          <div class="container">Container</div>
+     </main>
+     <footer class="footer">
+          FOOTER
+     </footer>
+</body>
+
+</html>
